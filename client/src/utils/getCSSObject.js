f6b6164875958bf1af5fd5ea55a8923d64fe7e61
aadityabhusal <bhusal.001aditya@gmil.com>
2021-10-22 19:06:06 +0545

    fix: modifies css text to object result to return array instead of object

--- client/src/utils/getCSSObject.js
+++ client/src/utils/getCSSObject.js
@@ -1,12 +1,13 @@
-export function getCSSText(styleText) {
-  let object = {};
-  let removedNewLine = styleText.replace(/[\n\r]+/g, "").split(";");
-
-  removedNewLine.forEach((item) => {
-    if (item) {
-      let prop = item.split(":");
-      object[prop[0].trim()] = prop[1].trim();
-    }
-  });
-  return object;
+export function getCSSArray(styleText) {
+  let results = [];
+  styleText
+    .replace(/[\n\r]+/g, "")
+    .split(";")
+    .forEach((item) => {
+      if (item) {
+        let prop = item.split(":");
+        results.push([prop[0].trim(), prop[1].trim()]);
+      }
+    });
+  return results;
 }
