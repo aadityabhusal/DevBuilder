0e7114ef2ebe5f7fe1369c5a5974586ba852aa95
aadityabhusal <bhusal.001aditya@gmil.com>
2021-10-22 14:36:32 +0545

    refactor: creates new util file for getCSSText and remove comment block from style editor

---
+++ client/src/utils/getCSSText.js
@@ -0,0 +1,10 @@
+export function getCSSText(styleBlocks) {
+  console.log("OK", styleBlocks);
+  let result = styleBlocks.map((block) => {
+    let list = block.style.map((item) => {
+      return `${item.name}:${item.value};`;
+    });
+    return `${block.selector}{${list.join("")}}`;
+  });
+  return result.join("");
+}
