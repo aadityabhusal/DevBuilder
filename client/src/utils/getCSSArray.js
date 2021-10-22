b52162f28c95c727dcf9ea25c006d83721c10bc1
aadityabhusal <bhusal.001aditya@gmil.com>
2021-10-22 19:32:31 +0545

    refactor: adds error handling to getCSSArray function

--- client/src/utils/getCSSArray.js
+++ client/src/utils/getCSSArray.js
@@ -1,13 +1,17 @@
 export function getCSSArray(styleText) {
-  let results = [];
-  styleText
-    .replace(/[\n\r]+/g, "")
-    .split(";")
-    .forEach((item) => {
-      if (item) {
-        let prop = item.split(":");
-        results.push([prop[0].trim(), prop[1].trim()]);
-      }
-    });
-  return results;
+  try {
+    let results = [];
+    styleText
+      .replace(/[\n\r]+/g, "")
+      .split(";")
+      .forEach((item) => {
+        if (item) {
+          let prop = item.split(":");
+          results.push([prop[0].trim(), prop[1].trim()]);
+        }
+      });
+    return results;
+  } catch (error) {
+    return null;
+  }
 }
