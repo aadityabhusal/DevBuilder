38ba053c0230574f753f4f8369b5ac1f3b305848
aadityabhusal <bhusal.001aditya@gmil.com>
2021-10-31 19:13:34 +0545

    refactor: adds secuity middlewares in express app

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,4 +1,3 @@
-require("dotenv").config({ path: __dirname + "/../.env" });
 const User = require("../models/userModel");
 const Site = require("../models/siteModel");
 const Page = require("../models/pageModel");
