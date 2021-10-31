265044e494a90fd6ff238522bc8bb15aa7a382ac
aadityabhusal <bhusal.001aditya@gmil.com>
2021-10-31 22:03:19 +0545

    feature: creates hashed passwords for register

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -31,6 +31,16 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
+UserSchema.pre("save", async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
 UserSchema.pre("findOneAndUpdate", function (next) {
   this.options.runValidators = true;
   next();
