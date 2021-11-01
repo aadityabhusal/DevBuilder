21eab70caf54ce418da42deae81dab7afdee8f6e
aadityabhusal <bhusal.001aditya@gmil.com>
2021-11-01 11:04:09 +0545

    feature: implements JWT access token verification

--- server/src/helpers/jwt.js
+++ server/src/helpers/jwt.js
@@ -11,7 +11,6 @@ function signAccessToken(_id) {
     };
     JWT.sign(payload, secret, options, (err, token) => {
       if (err) {
-        console.log(err);
         let error = new Error("Internal Server Error");
         error.status = 500;
         return reject(error);
@@ -21,6 +20,26 @@ function signAccessToken(_id) {
   });
 }
 
+function verifyAccessToken(req, res, next) {
+  try {
+    if (!req.headers["authorization"]) throw new Error("");
+    const token = req.headers["authorization"].split(" ")[1];
+    JWT.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
+      if (err) {
+        if (err.name === "JsonWebTokenError") throw new Error("");
+        else throw new Error(err.message);
+      }
+      req.payload = payload;
+      next();
+    });
+  } catch (err) {
+    if (err.message === "") err.message = "Unauthorized Access Error";
+    err.status = 401;
+    next(err);
+  }
+}
+
 module.exports = {
   signAccessToken,
+  verifyAccessToken,
 };
