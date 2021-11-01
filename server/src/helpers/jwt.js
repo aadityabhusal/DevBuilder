775514bfab32e996b20be42d0b8c9c58432f846a
aadityabhusal <bhusal.001aditya@gmil.com>
2021-11-01 12:58:36 +0545

    feature: implements refresh token functionality in backend

--- server/src/helpers/jwt.js
+++ server/src/helpers/jwt.js
@@ -10,11 +10,22 @@ function signAccessToken(_id) {
       audience: _id,
     };
     JWT.sign(payload, secret, options, (err, token) => {
-      if (err) {
-        let error = new Error("Internal Server Error");
-        error.status = 500;
-        return reject(error);
-      }
+      if (err) return reject(new Error("Internal Server Error"));
+      resolve(token);
+    });
+  });
+}
+function signRefreshToken(_id) {
+  return new Promise((resolve, reject) => {
+    const payload = { _id };
+    const secret = process.env.REFRESH_TOKEN_SECRET;
+    const options = {
+      expiresIn: "1y",
+      issuer: "devbuilder",
+      audience: _id,
+    };
+    JWT.sign(payload, secret, options, (err, token) => {
+      if (err) return reject(new Error("Internal Server Error"));
       resolve(token);
     });
   });
@@ -39,7 +50,22 @@ function verifyAccessToken(req, res, next) {
   }
 }
 
+function verifyRefreshToken(token) {
+  return new Promise((resolve, reject) => {
+    JWT.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, payload) => {
+      if (err) {
+        let error = new Error("Unauthorized Access Error");
+        error.status = 401;
+        return reject(error);
+      }
+      resolve(payload._id);
+    });
+  });
+}
+
 module.exports = {
   signAccessToken,
+  signRefreshToken,
   verifyAccessToken,
+  verifyRefreshToken,
 };
