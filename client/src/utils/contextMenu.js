e4f7558cbb73f18b852217a0a2366c5a93864de6
aadityabhusal <bhusal.001aditya@gmail.com>
2021-11-12 14:54:31 +0545

    fix: solves context menu top position to adjust with header absence

--- client/src/utils/contextMenu.js
+++ client/src/utils/contextMenu.js
@@ -9,9 +9,9 @@ export function showContextMenu(e) {
   let leftPanelWidth = document.getElementById("left-panel").clientWidth + 10;
   let top = e.clientY + 40;
   let left = e.clientX;
-  let yDiff = e.clientY + 210 - window.innerHeight;
+  let yDiff = e.clientY + 170 - window.innerHeight;
   let xDiff = e.clientX + leftPanelWidth + 100 - window.innerWidth;
-  if (yDiff > 0) top = window.innerHeight - 180;
+  if (yDiff > 0) top = window.innerHeight - 140;
   if (xDiff > 0) left = window.innerWidth - leftPanelWidth - 100;
   console.log(e.clientX + leftPanelWidth + 100, window.innerWidth);
   contextMenu.style.transform = `translate(${left}px,${top}px)`;
